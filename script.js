1ba1a27217722108194a31386e8b4cfc697bc530
Marvin Andrade <65877822+marvincris18@users.noreply.github.com>
2025-04-11 10:34:50 +0800

    Complete getComputerChoice
    function returns wither Rock, Scissors, or Paper randomly

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+
+function getComputerChoice() {
+    let computerChoice = "";
+    const computerRandom = Math.floor(Math.random() * 300) + 1;
+    if (computerRandom > 200) {
+        computerChoice = "Rock";
+    } else if (computerRandom > 100) {
+        computerChoice = "Scissors";
+    } else {
+        computerChoice = "Paper";
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
